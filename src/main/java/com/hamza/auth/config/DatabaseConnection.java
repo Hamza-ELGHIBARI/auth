3c1e1f30f6b73d463b71fb4e0e47bfc3250e30cb
Hamza EL GHIBARI <hamza.elghibari@gmail.com>
2025-03-23 01:03:14 +0000

    port

--- src/main/java/com/hamza/auth/config/DatabaseConnection.java
+++ src/main/java/com/hamza/auth/config/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 	
-    private static final String URL = "jdbc:mysql://localhost:3306/auth";
+    private static final String URL = "jdbc:mysql://localhost:3310/auth";
     private static final String USER = "root";  // Remplace par ton utilisateur
     private static final String PASSWORD = ""; // Remplace par ton mot de passe
 
